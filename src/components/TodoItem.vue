0f896b16572c0767543b967dc0391a7d2612e2b1
Govind Vishwakarma <govind.vishwakarma@unotechsoft.com>
2020-03-19 17:54:38 +0530

    Initial commit with add and delete functionality.

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}" >
+    <p>
+      <input type="checkbox" v-on:change="markComplete">
+      {{todo.title}}
+      <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['todo'],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .todo-item {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px dotted #ccc;
+  }
+  .is-complete {
+    text-decoration: line-through;
+  }
+  .del {
+      background: #ff0000;
+      color: #fff;
+      border: none;
+      border-radius: 50%;
+      float: right;
+      padding: 5px 9px;
+      cursor: pointer;      
+  }
+</style>
---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header class="header">
+        <h1>TodoList</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+    .header {
+        background: #333;
+        color: #FFF;
+        padding: 10px;
+        text-align: center;
+    }
+    .header a {
+        color: #fff;
+        padding-right: 5px;
+    }
+</style>
